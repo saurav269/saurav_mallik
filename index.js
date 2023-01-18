16f105b517a2759745402f07d4d323cfe2de006b
Saurav Mallik <sauravmallik786@gmail.com>
2023-01-18 18:05:58 +0530

    data

---
+++ index.js
@@ -0,0 +1,11 @@
+//Server.js
+
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 8080;
+server.use(router)
+server.listen(port, () => {
+    console.log("JSON Server is running",port)
+});
